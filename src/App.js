2ecfaa52f23ba295b603f2663e932ebaba49ba68
quynhtran158 <quynhtran.3002@gmail.com>
2024-03-06 21:00:42 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./Component/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
+      {/* <Services /> */}
       <Footer />
+      {/* <DoctorCard /> */}
     </div>
   );
 }
